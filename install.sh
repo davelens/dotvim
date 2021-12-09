52fa81760a33c5a972ea764aaa4041114dee1a40
davelens <github@davelens.be>
2021-12-09 19:58:24 +0100

    Make sure the install script creates a `~/.config` folder.

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # Stops further execution after any error.
 set -e
 
+[ ! -d ${HOME}/.config ] && mkdir ${HOME}/.config
+
 ln -s ${HOME}/.dotvim/config/nvim ${HOME}/.config/nvim
 ln -s ${HOME}/.dotvim/vim ${HOME}/.vim
 ln -s ${HOME}/.dotvim/vimrc ${HOME}/.vimrc
