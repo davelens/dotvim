7fad9157d4bc6fc5e3e725517eebf867c9ebfb2b
davelens <github@davelens.be>
2024-09-02 17:22:46 +0200

    Move lazy.nvim to lazy.lua.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,19 +3,7 @@
 vim.g.mapleader = " " -- using space as leader key
 vim.g.maplocalleader = "," -- using comma as local leader
 
--- bootstrap lazy.nvim plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
+require("lazy")
 
 local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
 vim.cmd.source(vimrc)
