f7865e51d89746ec01fae00ef5248f84490b3c25
davelens <github@davelens.be>
2024-09-05 15:55:08 +0200

    dvim: Add tpope/vim-repeat.

--- config/dvim/lua/plugins/init.lua
+++ config/dvim/lua/plugins/init.lua
@@ -9,6 +9,7 @@ return {
   'preservim/nerdcommenter', -- Comment toggles with <leader>ci
   'tpope/vim-surround', -- Maps to manipulate brackets, parens, quotes,..
   'tpope/vim-endwise', -- Smart end structures for blocks
+  'tpope/vim-repeat', -- Extended repeat functionality through `.`
   {
     "tpope/vim-fugitive", -- Git wrapper for vim
     keys = { { "<leader>g", "<cmd>G<cr>" } },
