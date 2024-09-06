08b22f03b49f35f68b19b51ffe892eeec9269bc6
davelens <github@davelens.be>
2024-09-06 14:51:28 +0200

    dvim: Fix the Trouble + Telescope keymaps.

--- config/dvim/lua/plugins/telescope.lua
+++ config/dvim/lua/plugins/telescope.lua
@@ -3,15 +3,15 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { 'folke/trouble.nvim' },
     { 'nvim-tree/nvim-web-devicons' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'folke/trouble.nvim', opts = {}, cmd = "Trouble" },
   },
   config = function()
     local telescope = require('telescope')
+    local trouble = require('trouble.sources.telescope')
     local builtin = require('telescope.builtin')
     local actions = require("telescope.actions")
-    local trouble = require('trouble.sources.telescope')
 
     vim.keymap.set('n', '<leader>t', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -23,13 +23,14 @@ return {
     telescope.setup {
       defaults = {
         mappings = {
-          -- Open selected files into a Trouble quicklist.
-          -- TODO: This worked, but not anymore. Conflict with a plugin probably.
-          i = { ['<c-t>'] = trouble.open },
-          n = { ['<c-t>'] = trouble.open },
-          -- Default is C-X, which I don't like.
-          i = { ['<c-s>'] = actions.select_horizontal },
-          n = { ['<c-s>'] = actions.select_horizontal },
+          i = { 
+            ['<c-t>'] = trouble.open, -- Open selected files into a Trouble quicklist.
+            ['<c-s>'] = actions.select_horizontal -- Default is C-X, which I don't like.
+          },
+          n = { 
+            ['<c-t>'] = trouble.open, -- Open selected files into a Trouble quicklist.
+            ['<c-s>'] = actions.select_horizontal -- Default is C-X, which I don't like.
+          }
           -- These are the defaults in junegunn's fzf.
           -- Commented because I want to use Telescope's defaults
           -- and not have them conflict with C-K / C-F.
