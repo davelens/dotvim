ec46c1989295e8c7590c43f05b2820f31b3abecb
davelens <github@davelens.be>
2024-09-06 16:14:25 +0200

    dvim: Make it so <leader>qc closes either Trouble or the quickfix list, depending which one is open. Trouble takes precedence though.

--- config/dvim/lua/plugins/trouble.lua
+++ config/dvim/lua/plugins/trouble.lua
@@ -3,7 +3,6 @@ return {
   opts = {}, 
   cmd = "Trouble",
   keys = {
-    { "<leader>qc", "<cmd>Trouble close<cr>", { silent = true } },
     { "]q", "<cmd>Trouble next<cr>", { silent = true } },
     { "[q", "<cmd>Trouble prev<cr>", { silent = true } },
     --{ "n", "<leader>xw", "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>" },
@@ -12,4 +11,29 @@ return {
     --{ "n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
     --{ "n", "gR", "<cmd>TroubleToggle lsp_references<cr>" },
   },
+  config = function()
+    local map = vim.keymap.set
+
+    local function is_trouble_open()
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
+        if filetype == "trouble" then
+          return true
+        end
+      end
+
+      return false
+    end
+
+    -- If Trouble is open, it will use the same 
+    map('n', "<leader>qc", function() 
+      if is_trouble_open() then
+        vim.cmd("Trouble close")
+      else
+        vim.cmd("ccl")
+      end
+    end, {})
+
+  end
 }
