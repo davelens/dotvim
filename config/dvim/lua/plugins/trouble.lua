84c1aa3ed9c88afc187d8b4e9f5279c2bd576d3f
davelens <github@davelens.be>
2024-09-06 17:19:24 +0200

    dvim: Make for easier file jumping when toggling Trouble from Telescope.

--- config/dvim/lua/plugins/trouble.lua
+++ config/dvim/lua/plugins/trouble.lua
@@ -3,8 +3,9 @@ return {
   opts = {}, 
   cmd = "Trouble",
   keys = {
-    { "]q", "<cmd>Trouble next<cr>", { silent = true } },
-    { "[q", "<cmd>Trouble prev<cr>", { silent = true } },
+    { "]t", "<cmd>Trouble next<cr><cmd>Trouble jump<cr>", { silent = true } },
+    { "[t", "<cmd>Trouble prev<cr><cmd>Trouble jump<cr>", { silent = true } },
+    { "]j", "<cmd>Trouble jump<cr>", { silent = true } },
     --{ "n", "<leader>xw", "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>" },
     --{ "n", "<leader>xd", "<cmd>TroubleToggle lsp_document_diagnostics<cr>" },
     --{ "n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
@@ -13,22 +14,14 @@ return {
   },
   config = function()
     local map = vim.keymap.set
+    local trouble = require('trouble')
 
-    local function is_trouble_open()
-      for _, win in ipairs(vim.api.nvim_list_wins()) do
-        local buf = vim.api.nvim_win_get_buf(win)
-        local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
-        if filetype == "trouble" then
-          return true
-        end
-      end
-
-      return false
-    end
-
-    -- If Trouble is open, it will use the same 
+    -- Just a generic command ot close both Trouble or the quickfix list.
+    -- Trouble windows take precedence if both are open.
+    -- NOTE: I might rebind this to something like ]c. I don't think 
+    -- vim-unimpaired uses c.
     map('n', "<leader>qc", function() 
-      if is_trouble_open() then
+      if trouble.is_open() then
         vim.cmd("Trouble close")
       else
         vim.cmd("ccl")
