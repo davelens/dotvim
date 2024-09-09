2e40916bb57d032152d6a0d69b4d63e2ea6227ac
davelens <github@davelens.be>
2024-09-09 13:39:08 +0200

    dvim: Add nvim-cmp and configure autocompletion with the necessary LSP as source.

--- config/dvim/lua/plugins/nvim-lspconfig.lua
+++ config/dvim/lua/plugins/nvim-lspconfig.lua
@@ -11,16 +11,33 @@ return {
     })
 
     local lspconfig = require('lspconfig')
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     lspconfig.lua_ls.setup({
       settings = {
         Lua = {
           diagnostics = {
             globals = { 'vim' },
-            disable = {'lowercase-global', 'trailing-space'}
+            disable = {'lowercase-global', 'trailing-space', 'undefined-global'}
           }
         }
       }
+    })
+
+    lspconfig.elixirls.setup({
+      cmd = { "elixir-ls" },
+      -- set default capabilities for nvim-cmp's completion source
+      capabilities = capabilities,
+    })
+
+    lspconfig.tailwindcss.setup({
+      init_options = {
+        userLanguages = {
+          elixir = "html-eex",
+          eelixir = "html-eex",
+          heex = "html-eex",
+        },
+      },
     })
 
     lspconfig.elixirls.setup({})
