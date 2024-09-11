03daccd85e0c714f073853f691edefa5f432764d
davelens <github@davelens.be>
2024-09-11 13:05:05 +0200

    dvim: Change Telescope's <leader-t> bind to :Telescope git_files instead of find_files.

--- config/dvim/lua/plugins/telescope.lua
+++ config/dvim/lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
     local builtin = require('telescope.builtin')
     local actions = require("telescope.actions")
 
-    vim.keymap.set('n', '<leader>t', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>t', builtin.git_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
