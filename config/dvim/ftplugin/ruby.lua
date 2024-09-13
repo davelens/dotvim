3521d251993a516b3331c017f6b77757576a48ee
davelens <github@davelens.be>
2024-09-13 15:00:24 +0200

    dvim: Cleanup the ruby.lua and move certain autocommands to neovim/autocommands for now.

--- config/dvim/ftplugin/ruby.lua
+++ config/dvim/ftplugin/ruby.lua
@@ -11,29 +11,17 @@ vim.opt_local.smarttab = true
 vim.opt_local.softtabstop = 2 -- Determines how much whitespace is used while indenting
 vim.opt_local.tabstop = 2 -- The width of a tab character
 
-vim.api.nvim_create_autocmd('BufEnter', {
-  pattern = '*.rb,*.erb,*.html.erb',
-  callback = function(args)
-    map = vim.keymap.set
-    default_opts = { noremap = true, silent = false, buffer = args.buf }
+map = vim.api.nvim_buf_set_keymap
+default_opts = { noremap = true, silent = false }
 
-    -- rails.vim
-    map('n', '<leader>a', '<cmd>A<CR>', default_opts)
-    map('n', '<leader>r', '<cmd>R<CR>', default_opts)
-        
-    -- Telescope
-    -- Method definition lookup. Same as <leader>l, but prefixes search string with "def "
-    map('n', '<leader>d', ":<C-U>lua require('telescope.builtin').grep_string({search = 'def ' .. vim.fn.expand('<cword>')})<CR>", default_opts)
-    map('v', '<leader>d', ":<C-U>lua require('telescope.builtin').grep_string({search = 'def ' .. get_visual_selection()})<CR>", default_opts)
+-- rails.vim
+map(0, 'n', '<leader>a', '<cmd>A<CR>', default_opts)
+map(0, 'n', '<leader>r', '<cmd>R<CR>', default_opts)
 
-    -- vim-localorie bindings
-    map('n', '<leader>yt', ":lua vim.fn['localorie#translate']()<CR>", default_opts)
-    map('n', '<leader>ye', ":lua print(vim.fn['localorie#expand_key']())<CR>", default_opts)
-    --map('v', '<leader>yt', ":lua vim.fn['localorie#translate']()<CR>", default_opts)
-    --map('v', '<leader>i', ':<C-U>lua print(get_visual_selection())<CR>', { noremap = true, silent = true, buffer = true })
-    --vnoremap <leader>i "ky:echo system("~/.bin/rails/lookup-translations ". @k)<CR>
-  end
-})
+-- Telescope
+-- Method definition lookup. Same as <leader>l, but prefixes search string with "def "
+map(0, 'n', '<leader>d', ":<C-U>lua require('telescope.builtin').grep_string({search = 'def ' .. vim.fn.expand('<cword>')})<CR>", default_opts)
+map(0, 'v', '<leader>d', ":<C-U>lua require('telescope.builtin').grep_string({search = 'def ' .. get_visual_selection()})<CR>", default_opts)
 
 local rails = vim.api.nvim_create_augroup('rails', { clear = true })
 
--- config/dvim/lua/neovim/autocommands.lua
+++ config/dvim/lua/neovim/autocommands.lua
@@ -19,3 +19,16 @@ vim.api.nvim_create_autocmd("UILeave", {
     io.write("\027]111\027\\")
   end,
 })
+
+-- vim-localorie bindings need to be in multiple Ruby-related buffers.
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'ruby,eruby',
+  callback = function(_)
+    map = vim.api.nvim_buf_set_keymap
+    default_opts = { noremap = true, silent = false }
+    map(0, 'n', '<leader>yt', ":lua vim.fn['localorie#translate']()<CR>", default_opts)
+    map(0, 'n', '<leader>ye', ":lua print(vim.fn['localorie#expand_key']())<CR>", default_opts)
+    map(0, 'v', '<leader>yt', ":lua vim.fn['localorie#translate']()<CR>", default_opts)
+  end
+})
+
