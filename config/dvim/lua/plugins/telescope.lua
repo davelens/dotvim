381a668e4867c4cb300a0731cba9010501ff3a29
davelens <github@davelens.be>
2024-09-17 13:53:22 +0200

    dvim: Housekeeping

--- config/dvim/lua/plugins/telescope.lua
+++ config/dvim/lua/plugins/telescope.lua
@@ -2,10 +2,10 @@
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
-    { 'nvim-lua/plenary.nvim' },
-    { 'nvim-tree/nvim-web-devicons' },
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'nvim-telescope/telescope-live-grep-args.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { 'nvim-telescope/telescope-live-grep-args.nvim' },
   },
   config = function()
     local telescope = require('telescope')
