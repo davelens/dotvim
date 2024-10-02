087920374aefddcd24de8dd57fbd3a6a2cbc3d20
davelens <github@davelens.be>
2024-10-02 17:39:02 +0200

    dvim: Alphabetize plugins/basic.lua

--- config/dvim/lua/plugins/basic.lua
+++ config/dvim/lua/plugins/basic.lua
@@ -1,10 +1,10 @@
 return {
+  'airblade/vim-localorie', -- Maps and functions for Rails I18n interaction
   'preservim/nerdcommenter', -- Comment toggles with <leader>ci
-  'tpope/vim-surround', -- Maps to manipulate brackets, parens, quotes,..
+  'tpope/vim-dispatch', -- Asynchronous build and test dispatcher
   'tpope/vim-endwise', -- Smart end structures for blocks
   'tpope/vim-repeat', -- Extended repeat functionality through `.`
   'tpope/vim-rails', -- Pandora's box with Rails workflow features
+  'tpope/vim-surround', -- Maps to manipulate brackets, parens, quotes,..
   'tpope/vim-unimpaired', -- Complementary maps for quickfix, lists, options
-  'tpope/vim-dispatch', -- Asynchronous build and test dispatcher
-  'airblade/vim-localorie', -- Maps and functions for Rails I18n interaction
 }
