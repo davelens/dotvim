0a42f21fef254de1d474cd6f0a526453693e91d1
davelens <github@davelens.be>
2024-10-02 16:13:07 +0200

    dvim: Use less interfering keymaps for git pull (gp) and git push (gu) in the vim-fugitive window. "P" was "edit patch".

--- config/dvim/lua/plugins/autoload/vim-fugitive.lua
+++ config/dvim/lua/plugins/autoload/vim-fugitive.lua
@@ -8,14 +8,14 @@ return {
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "fugitive",
       callback = function()
-        vim.api.nvim_buf_set_keymap(0, 'n', 'P', ':Git pull<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(0, 'n', 'gu', ':Git pull<CR>', { noremap = true, silent = true })
       end,
     })
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "fugitive",
       callback = function()
-        vim.api.nvim_buf_set_keymap(0, 'n', 'U', ':Git push<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(0, 'n', 'gp', ':Git push<CR>', { noremap = true, silent = true })
       end,
     })
   end
