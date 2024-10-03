78fbd05ce64c998332b460733a59b7755db1488c
davelens <github@davelens.be>
2024-10-03 03:17:11 +0200

    dvim: Alphabetize the options.

--- config/dvim/lua/homebrew/options.lua
+++ config/dvim/lua/homebrew/options.lua
@@ -7,21 +7,19 @@ vim.opt.shiftwidth = 2 -- Use 2 spaces as default indent width when using << >>
 vim.opt.smartindent = true -- Smart(ish) autoindenting when starting a new line
 vim.opt.softtabstop = 2 -- Determines how much whitespace is used while indenting
 vim.opt.tabstop = 2 -- The width of a tab character
--- Inserts/Deletes whitespace in front of lines according to the above settings
-vim.opt_local.smarttab = true
+vim.opt_local.smarttab = true -- Inserts/Deletes whitespace in front of lines according to the above settings
 
 -- Behavioural settings
+vim.colorscheme = 'onenord' -- Set the colorscheme
 vim.opt.autoread = true -- Load in changes made from *outside* vim
 vim.opt.autowrite = true -- Write file contents for writable buffers
--- Allow backspacing over autoindent, line breaks and insert mode start
-vim.opt.backspace = 'indent,eol,start'
+vim.opt.backspace = 'indent,eol,start' -- Allow backspacing over autoindent, line breaks and insert mode start
+vim.opt.clipboard = 'unnamedplus' -- Use the system clipboard
 vim.opt.colorcolumn = '80' -- Mark a (convenient) vertical line at the 80 char mark
 vim.opt.completeopt = 'menu,longest' -- Autocompletion menu with the longest common match
+vim.opt.cursorcolumn = false -- Adds a coloured vertical (distracting) cursor column
 vim.opt.cursorline = true -- Adds a coloured horizontal (convenient) cursor row
--- TODO: See about using vim.opt.undofile + simnalamburt/vim-mundo instead.
-vim.opt.directory = vim.fn.expand("~") .. "/.local/share/dvim/swap" --The swapfile directory
 vim.opt.encoding = 'utf-8'
---vim.opt.fileformat = unix -- Use <NL> as EOL
 vim.opt.foldclose = 'all' -- When inactive folds get closed when foldmethod is toggled
 vim.opt.foldmethod = 'marker' -- Only fold by means of inline marker comments
 vim.opt.hidden = true -- Keeps buffers in the background when left behind
@@ -29,7 +27,6 @@ vim.opt.ignorecase = true -- Ignore casing in pattern matching
 vim.opt.incsearch = true -- Highlight search matches as you type
 vim.opt.laststatus = 2 -- Always show a statusline
 vim.opt.linespace = 0 -- Number of pixel lines between characters
-vim.opt.cursorcolumn = false -- Adds a coloured vertical (distracting) cursor column
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.scrolloff = 10 -- Keep 10 lines above and below the cursor when scrolling
@@ -39,5 +36,8 @@ vim.opt.smartcase = true -- Ignores casing when all pattern characters are lower
 vim.opt.updatetime = 250 -- Lower-than-default wait time (in ms) before writing swap file
 vim.opt.visualbell = true -- NO BEEPING
 vim.opt.whichwrap:append('<,>,[,]') -- Allows word wrapping only when cursor keys are used
-vim.opt.clipboard = 'unnamedplus' -- Use the system clipboard
-vim.colorscheme = 'onenord' -- Set the colorscheme
+
+-- TODO: See about using vim.opt.undofile + simnalamburt/vim-mundo instead.
+vim.opt.directory = vim.fn.expand("~") .. "/.local/share/dvim/swap" --The swapfile directory
+
+
