8fe9c1849deeb73d7add27b74595f36c25fdf399
davelens <github@davelens.be>
2024-10-03 03:20:26 +0200

    dvim: Colorize the Copilot suggestions in a colour different from comments. This should be less confusing

--- config/dvim/lua/plugins/autoload/copilot.lua
+++ config/dvim/lua/plugins/autoload/copilot.lua
@@ -2,6 +2,9 @@
 return {
   'zbirenbaum/copilot.lua', 
   config = function()
+    vim.api.nvim_set_hl(0, 'CopilotSuggestion', { fg = '#83a598' })
+    vim.api.nvim_set_hl(0, 'CopilotAnnotation', { fg = '#83a598' })
+
     require('copilot').setup({
       suggestion = {
         auto_trigger = true,
