0b8aed9a479bbabcf1750fa4a7b559249b36122f
davelens <github@davelens.be>
2024-12-23 15:43:54 +0100

    Review my install/uninstall script with nerdfont configuration, and let them provide more meaningful feedback.

--- install.sh
+++ install.sh
@@ -3,13 +3,38 @@
 # Stops further execution after any error.
 set -e
 
-[ ! -d ${HOME}/.config ] && mkdir ${HOME}/.config
+source helpers.sh
+
 [ ! -d ${HOME}/.local/bin ] && mkdir -p ${HOME}/.local/bin
 
-[ ! -f ${HOME}/.config/nvim ] && ln -s ${HOME}/.dotvim/config/nvim ${HOME}/.config/nvim
-[ ! -f ${HOME}/.config/dvim ] && ln -s ${HOME}/.dotvim/config/dvim ${HOME}/.config/dvim
+# Check if ~/.local/bin is in $PATH so we can install getnf if necessary.
+if [[ ! $(echo $PATH | tr ':' '\n' | grep -x "$HOME/.local/bin") ]]; then
+  echo "$(cross) ERROR: ~/.local/bin needs to be in your \$PATH."
+  exit
+fi
+
+echo "$(check) ~/.local/bin is in \$PATH"
+
+# Install getnf if not already installed.
+if [ ! -f ${HOME}/.local/bin/getnf ]; then
+  curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+fi
+
+echo "$(check) getnf is installed"
+
+[ ! -d ${HOME}/Library/Fonts/DroidSansMono ] && getnf -i DroidSansMono 
+[ ! -d ${HOME}/Library/Fonts/Hack ] && getnf -i Hack 
+
+echo "$(check) Nerd fonts installed"
+
+[ ! -d ${HOME}/.config ] && mkdir ${HOME}/.config
+
+[ ! -d ${HOME}/.config/nvim ] && ln -s ${HOME}/.dotvim/config/nvim ${HOME}/.config/nvim
+[ ! -d ${HOME}/.config/dvim ] && ln -s ${HOME}/.dotvim/config/dvim ${HOME}/.config/dvim
 [ ! -d ${HOME}/.vim ] && ln -s ${HOME}/.dotvim/vim ${HOME}/.vim
 [ ! -f ${HOME}/.vimrc ] && ln -s ${HOME}/.dotvim/vimrc ${HOME}/.vimrc
+
+echo "$(check) Symlinks are present in ~/.config"
 
 if [ ! -f ${HOME}/.local/bin/dvim ]; then
   cat >> ${HOME}/.local/bin/dvim<< EOF
@@ -21,4 +46,7 @@ exec -a "\$NVIM_APPNAME" nvim -u "~/.config/dvim/init.lua" "\$@"
 echo 'Done.'
 EOF
   chmod +x ${HOME}/.local/bin/dvim
+  echo "$(check) Created ~/.local/bin/dvim executable"
 fi
+
+echo "$(check) dvim successfully installed"
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# Stops further execution after any error.
+set -e
+
+source helpers.sh
+
+# nvim uninstall
 [ -L ~/.config/nvim ] && rm ~/.config/nvim
 [ -L ~/.vim ] && rm ~/.vim
 [ -L ~/.vimrc ] && rm ~/.vimrc
 [ -f ~/Library/Fonts/DroidSansMonoForPowerlineNerdFontComplete.otf ] && rm ~/Library/Fonts/DroidSansMonoForPowerlineNerdFontComplete.otf
 
-echo 'Done.';
+# dvim uninstall
+[[ getnf -u DroidSansMono,Hack ]] && echo "$(check) Nerd fonts uninstalled"
+[ -f ~/.local/bin/dvim ] && rm ~/.local/bin/dvim && echo "$(check) Removed dvim executable"
+
+echo "$(check) dvim successfully uninstalled"
