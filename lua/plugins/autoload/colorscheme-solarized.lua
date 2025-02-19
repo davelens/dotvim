95288a254be30caef4334dbde9d4ba422f7457dc
davelens <github@davelens.be>
2025-02-19 22:56:58 +0100

    Remove solarized and onenord, sticking to catppuccin for now.

--- lua/plugins/autoload/colorscheme-solarized.lua
+++
@@ -1,9 +0,0 @@
--- Pretty and/or healthy colours for long programming sessions.
-return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  --config = function()
-    --vim.cmd[[colorscheme solarized-osaka]]
-  --end
-}
