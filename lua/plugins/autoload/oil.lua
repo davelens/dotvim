7d31a5f2dcc7dcf0a9a48f3af0e83824ac614590
davelens <github@davelens.be>
2025-02-19 17:04:35 +0100

    Give Oil a fair shot before dismissing it and give it a dedicated keybind (--float by default so I can still use `q` to close it).

--- lua/plugins/autoload/oil.lua
+++ lua/plugins/autoload/oil.lua
@@ -2,8 +2,13 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   opts = {},
+  keys = {
+    {
+      "<leader>o",
+      "<cmd>Oil --float<CR>",
+      silent = true,
+      desc = 'Open [O]il.nvim'
+    },
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require("oil").setup()
-  end,
 }
