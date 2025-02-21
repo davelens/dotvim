f356749b97daa3a4973c2c29637818bc07b28903
davelens <github@davelens.be>
2025-02-21 01:05:44 +0100

    Restructure the let assignment to make it more readable.

--- lua/homebrew/functions/rspec.lua
+++ lua/homebrew/functions/rspec.lua
@@ -1,19 +1,15 @@
 local rspec = {}
 
 -- Converts "foo = create(:bar) to foo { create(:bar) }"
+--
+-- I haven't found this to be easier in pure Lua.
+-- You could probably do it in a oneliner with regex, but
 function rspec.refactor_assignment_to_let()
-  -- Delete the current line
-  vim.cmd('normal! dd')
-
-  -- Paste the deleted line above the nearest 'it' block
-  vim.cmd('normal! ?^%s*it\\>')  -- Search for the next 'it' block
-  vim.cmd('normal! P')            -- Paste the deleted line
-
-  -- Substitute the variable assignment with an RSpec let statement
-  vim.cmd([[.s/\(\w\+\) = \(.*\)$/let(:\1) { \2 }/]])
-
-  -- Indent the line properly
-  vim.cmd('normal ==')
+  vim.cmd('normal! dd') -- Delete the current line
+  vim.cmd('normal! ?^%s*it\\>') -- Search for the next 'it' block
+  vim.cmd('normal! P') -- Paste the deleted line above it
+  vim.cmd([[.s/\(\w\+\) = \(.*\)$/let(:\1) { \2 }/]]) -- Do the substitution
+  vim.cmd('normal ==') -- Fix indentation
 end
 
 return rspec
