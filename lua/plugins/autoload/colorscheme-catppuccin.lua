cef3f41f41b27c9c543615d2d623c2b07b004f8d
davelens <github@davelens.be>
2025-02-23 15:13:01 +0100

    Make it so we can override highlights using the pre-defined catppuccin colors we get from the callback.

--- lua/plugins/autoload/colorscheme-catppuccin.lua
+++ lua/plugins/autoload/colorscheme-catppuccin.lua
@@ -4,11 +4,15 @@ return {
   name = "catppuccin",
   priority = 1000,
   opts = {
-    custom_highlights = {
-      -- zbirenbaum/copilot.lua
-      CopilotSuggestion = { fg = '#83a598' },
-      CopilotAnnotation = { fg = '#83a598' }
-    },
+    custom_highlights = function(colors)
+      return {
+        -- folke/lazy.nvim
+        IncSearch = { bg = colors.lavender },
+        -- zbirenbaum/copilot.lua
+        CopilotSuggestion = { fg = '#83a598' },
+        CopilotAnnotation = { fg = '#83a598' }
+      }
+    end,
   },
   init = function()
     vim.cmd.colorscheme "catppuccin"
