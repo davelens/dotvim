2017bb96c0c0b2669763304fe2ca2391b84fbd39
davelens <github@davelens.be>
2025-02-26 00:14:19 +0100

    Remove unused <leader>fc for now.

--- lua/plugins/autoload/snacks.lua
+++ lua/plugins/autoload/snacks.lua
@@ -29,18 +29,17 @@ return {
     styles = { enabled = false },
   },
   keys = {
-    { "gd",         function() Snacks.picker.lsp_definitions() end,                         desc = "Goto Definition" },
+    { "gd",         function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
     --{ "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     --{ "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     --{ "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
 
-    { "<leader>fb", function() Snacks.picker.buffers() end,                                 desc = "Find [b]uffers" },
-    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find [C]onfig file" },
-    { "<leader>ff", function() Snacks.picker.files() end,                                   desc = "Find [f]iles" },
-    { "<leader>fg", function() Snacks.picker.grep() end,                                    desc = "[G]rep" },
-    { "<leader>fh", function() Snacks.picker.help() end,                                    desc = "Find [h]elp" },
-    { "<leader>fu", function() Snacks.picker.undo() end,                                    desc = "Find in [u]ndofile history", mode = { "n", "x" } },
-    { "<leader>fw", function() Snacks.picker.grep_word() end,                               desc = "Find [w]ord or selection",   mode = { "n", "x" } },
+    { "<leader>fb", function() Snacks.picker.buffers() end,         desc = "Find [b]uffers" },
+    { "<leader>ff", function() Snacks.picker.files() end,           desc = "Find [f]iles" },
+    { "<leader>fg", function() Snacks.picker.grep() end,            desc = "[G]rep" },
+    { "<leader>fh", function() Snacks.picker.help() end,            desc = "Find [h]elp" },
+    { "<leader>fu", function() Snacks.picker.undo() end,            desc = "Find in [u]ndofile history", mode = { "n", "x" } },
+    { "<leader>fw", function() Snacks.picker.grep_word() end,       desc = "Find [w]ord or selection",   mode = { "n", "x" } },
 
     {
       "<leader>fd",
