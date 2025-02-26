76ed189c6eab9a3e3bac8b9a406a171e8e88b3a3
davelens <github@davelens.be>
2025-02-26 15:49:56 +0100

    Expand the events on which RSpec functions are unloaded from the dvim object.

--- lua/config/functions/rspec.lua
+++ lua/config/functions/rspec.lua
@@ -5,11 +5,11 @@ local rspec = {}
 -- I haven't found this to be easier in pure Lua.
 -- You could probably do it in a oneliner with regex, but
 function rspec.refactor_assignment_to_let()
-  vim.cmd('normal! dd') -- Delete the current line
-  vim.cmd('normal! ?^%s*it\\>') -- Search for the next 'it' block
-  vim.cmd('normal! P') -- Paste the deleted line above it
+  vim.cmd('normal! dd')                               -- Delete the current line
+  vim.cmd('normal! ?^%s*it\\>')                       -- Search for the next 'it' block
+  vim.cmd('normal! P')                                -- Paste the deleted line above it
   vim.cmd([[.s/\(\w\+\) = \(.*\)$/let(:\1) { \2 }/]]) -- Do the substitution
-  vim.cmd('normal ==') -- Fix indentation
+  vim.cmd('normal ==')                                -- Fix indentation
 end
 
 -- Load the autocommands for the RSpec filetype buffers.
@@ -27,7 +27,7 @@ function rspec.load_autocommands(callback)
     end
   })
 
-  autocmd('BufLeave', {
+  autocmd({ 'BufLeave', 'BufDelete', 'FocusLost' }, {
     group = rspec,
     pattern = '*_spec.rb',
     callback = function(_)
