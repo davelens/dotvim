2e507b99a770d227f58e809e489a16947ac5c379
davelens <github@davelens.be>
2025-03-06 14:38:55 +0100

    functions/init: Add support for calling dvim.load() with a string argument representing the module to load in, and make it so other events are possible to pass in (+ some housekeeping).

--- lua/config/functions/init.lua
+++ lua/config/functions/init.lua
@@ -1,10 +1,48 @@
+-- This function is local so it doesn't get exposed on _G.dvim.
+local function setup_dvim_autocommands(opts, callback_opts)
+  if not opts then
+    return
+  end
+
+  local group = augroup(opts.name)
+
+  autocmd(opts.event, {
+    group = group,
+    pattern = opts.pattern,
+    callback = function(_)
+      dd('Running autocommands for' .. opts.name)
+      opts.autocommands(callback_opts)
+    end
+  })
+
+  if opts.event == 'BufEnter' then
+    autocmd({ 'BufLeave', 'BufDelete', 'FocusLost' }, {
+      group = group,
+      pattern = opts.pattern,
+      callback = function(_)
+        dvim.unload(opts.name)
+      end
+    })
+  end
+end
+
 _G.dvim = {
   -- Load a submodule and stores it in the global dvim object
   load = function(opts)
     local src
+
+    if not opts then
+      error('dvim.load requires a module name or options table')
+    end
+
+    if type(opts) == 'string' then
+      opts = { module = opts }
+    end
+
     opts = vim.tbl_extend('force', {
       name = nil,
       module = nil,
+      event = 'BufEnter',
       pattern = nil,
       autocommands = nil,
     }, opts) or {}
@@ -12,24 +50,8 @@ _G.dvim = {
     opts.name = opts.name or opts.module:match('([^.]+)$')
     src = _G.dvim[opts.name] or require(opts.module)
 
-    group = augroup(opts.name)
-
     if opts.autocommands then
-      autocmd('BufEnter', {
-        group = group,
-        pattern = opts.pattern,
-        callback = function(_)
-          opts.autocommands(src)
-        end
-      })
-
-      autocmd({ 'BufLeave', 'BufDelete', 'FocusLost' }, {
-        group = group,
-        pattern = opts.pattern,
-        callback = function(_)
-          dvim.unload(opts.name)
-        end
-      })
+      setup_dvim_autocommands(opts, src)
     end
 
     _G.dvim[opts.name] = src
