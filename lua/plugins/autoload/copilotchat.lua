bbc880918ed63efeb976e119cf9c2dded94c5638
davelens <github@davelens.be>
2025-03-11 16:48:27 +0100

    Remove duplicate dependency.

--- lua/plugins/autoload/copilotchat.lua
+++ lua/plugins/autoload/copilotchat.lua
@@ -3,7 +3,6 @@ return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      { 'zbirenbaum/copilot.lua' },
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
     -- Only on MacOS or Linux
