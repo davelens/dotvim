4f4102017f05808009b1a7ccdd057556947b3b5d
davelens <github@davelens.be>
2025-04-05 22:32:19 +0200

    plugin/luasnip: Confirm the homebrew snippets folder and document inline.

--- lua/plugins/autoload/luasnip.lua
+++ lua/plugins/autoload/luasnip.lua
@@ -1,15 +1,17 @@
 return {
   'L3MON4D3/LuaSnip',
   version = 'v2.*',
-  lazy = true,
   build = 'make install_jsregexp',
   dependencies = {
     {
       'rafamadriz/friendly-snippets',
       config = function()
+        -- Loads vscode-like snippets (here: rafamadriz/friendly-snippets).
         require('luasnip.loaders.from_vscode').lazy_load()
+
+        -- Loads in my Homebrew snippets.
         require('luasnip.loaders.from_vscode').lazy_load({
-          paths = { vim.fn.stdpath('config') .. '/snippets' },
+          paths = { vim.fn.stdpath('config') .. '/snippets' }, -- Homebrew
         })
       end,
     },
