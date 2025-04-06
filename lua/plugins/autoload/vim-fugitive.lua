01f38a1e61fd13ada461a9c45d70ad911734057d
davelens <github@davelens.be>
2025-04-06 15:57:37 +0200

    Add some keymap descriptions for which-key.

--- lua/plugins/autoload/vim-fugitive.lua
+++ lua/plugins/autoload/vim-fugitive.lua
@@ -2,9 +2,9 @@
 return {
   'tpope/vim-fugitive',
   keys = {
-    { '<leader>g', '<cmd>G<cr>' },
-    { 'gu', '<cmd>Git pull<cr>', ft = 'fugitive' },
-    { 'gp', '<cmd>Git push<cr>', ft = 'fugitive' },
+    { '<leader>g', '<cmd>G<cr>', desc = 'Open vim-fu[g]itive window' },
+    { 'gu', '<cmd>Git pull<cr>', ft = 'fugitive', desc = '[G]it p[u]ll' },
+    { 'gp', '<cmd>Git push<cr>', ft = 'fugitive', desc = '[G]it [p]ush' },
   },
   -- no opts needed here since this is a pure vim plugin.
 }
--- lua/plugins/autoload/vim-rails.lua
+++ lua/plugins/autoload/vim-rails.lua
@@ -3,8 +3,18 @@ return {
   'tpope/vim-rails',
   ft = { 'ruby', 'eruby' },
   keys = {
-    { '<leader>a', '<cmd>A<cr>' },
-    { '<leader>r', '<cmd>R<cr>' },
+    {
+      '<leader>a',
+      '<cmd>A<cr>',
+      ft = { 'ruby', 'eruby' },
+      desc = 'Switch to [A]lternate file',
+    },
+    {
+      '<leader>r',
+      '<cmd>R<cr>',
+      ft = { 'ruby', 'eruby' },
+      desc = 'Switch to [R]elated file',
+    },
   },
   -- no opts needed here since this is a pure vim plugin.
 }
