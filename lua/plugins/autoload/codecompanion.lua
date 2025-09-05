8c2809bad57ea23c26eec4a1bfd9eb66874f2593
davelens <github@davelens.be>
2025-09-05 16:05:44 +0200

    plugins/codecompanion: Add in keymaps for the `cmd` commands.

--- lua/plugins/autoload/codecompanion.lua
+++ lua/plugins/autoload/codecompanion.lua
@@ -6,6 +6,24 @@ return {
     'CodeCompanionCmd',
     'CodeCompanionActions',
   },
+  keys = {
+    { ';cp', '<cmd>CodeCompanion', desc = '[C]odeCompanion: Basic [p]rompt' },
+    {
+      ';ca',
+      '<cmd>CodeCompanionActions<cr>',
+      desc = '[C]odeCompanion: Perform an [A]ction',
+    },
+    {
+      ';cc',
+      '<cmd>CodeCompanionChat<cr>',
+      desc = '[C]odeCompanion: Open [c]hat',
+    },
+    {
+      ';cr',
+      '<cmd>CodeCompanionCmd<cr>',
+      desc = '[C]odeCompanion: [R]un a shell command',
+    },
+  },
   opts = {
     opts = {
       log_level = 'ERROR', -- Here for when I need it changed to DEBUG quickly.
