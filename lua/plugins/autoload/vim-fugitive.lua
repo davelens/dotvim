4f828e493321d3877d1218937264c478a99bb6da
davelens <github@davelens.be>
2025-09-25 14:19:32 +0200

    plugins/vim-fugitive: Add a quick pull trigger in <leader>gU and reformat the keymaps for readability.

--- lua/plugins/autoload/vim-fugitive.lua
+++ lua/plugins/autoload/vim-fugitive.lua
@@ -4,31 +4,12 @@ return {
   cmd = 'Git',
   keys = {
     { ';g', '<cmd>G<cr>', desc = 'Git (vim-fugitive)' },
-    {
-      '<leader>ga',
-      '<cmd>G add %<cr>',
-      desc = 'Stage current buffer',
-    },
-    {
-      '<leader>gp',
-      '<cmd>G add -p %<cr>',
-      desc = 'Add patches to stage',
-    },
-    {
-      '<leader>gP',
-      '<cmd>G push<cr>',
-      desc = 'Push',
-    },
-    {
-      '<leader>gc',
-      '<cmd>G commit<cr>',
-      desc = 'Commit staged files',
-    },
-    {
-      '<leader>gr',
-      '<cmd>G rebase -i<cr>',
-      desc = 'Interactive rebase',
-    },
+    { '<leader>ga', '<cmd>G add %<cr>', desc = 'Stage current buffer' },
+    { '<leader>gp', '<cmd>G add -p %<cr>', desc = 'Add patches to stage' },
+    { '<leader>gP', '<cmd>G push<cr>', desc = 'Push' },
+    { '<leader>gU', '<cmd>G pull<cr>', desc = 'Pull' },
+    { '<leader>gc', '<cmd>G commit<cr>', desc = 'Commit staged files' },
+    { '<leader>gr', '<cmd>G rebase -i<cr>', desc = 'Interactive rebase' },
     { 'gu', '<cmd>Git pull<cr>', ft = 'fugitive', desc = '[G]it p[u]ll' },
     { 'gp', '<cmd>Git push<cr>', ft = 'fugitive', desc = '[G]it [p]ush' },
     { ';g', '<cmd>q<cr>', ft = 'fugitive', desc = 'Git (close)' },
