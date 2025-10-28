ac1300ced04c09d1c7ea86c25a375227e9bd08a6
davelens <github@davelens.be>
2025-10-28 12:09:43 +0100

    Rename config/maps.lua to config/keymaps.lua.

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require('config.globals')
 require('config.options')
 require('config.functions')
 require('config.autocommands')
-require('config.maps')
+require('config.keymaps')
 require('config.lazy').load({
   keymap = ';p',
   spec = {
