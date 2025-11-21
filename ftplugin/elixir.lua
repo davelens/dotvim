102d675afabff4d1a2e8c749c250fcbc80b7bc51
davelens <github@davelens.be>
2025-11-21 15:33:43 +0100

    config/elixir: Make it so my `wg` in visual mode works in both .heex and .ex so I can wrap gettext around strings in Liveview contexts.

--- ftplugin/elixir.lua
+++ ftplugin/elixir.lua
@@ -13,3 +13,18 @@ vim.keymap.set(
   '<cmd>lua toggle_elixir_runner()<CR>',
   { noremap = true, silent = true, buffer = true }
 )
+
+dvim.load({
+  module = 'config.functions.elixir',
+  pattern = { '*.ex', '*.html.heex' },
+  autocommands = function(heex)
+    vim.schedule(function()
+      vim.keymap.set(
+        'v',
+        'wg',
+        heex.wrap_with_gettext,
+        { noremap = true, silent = false, buffer = true }
+      )
+    end)
+  end,
+})
