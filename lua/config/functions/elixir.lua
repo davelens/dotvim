04b8740a1b9b72f745f649368f9d45d0c2815d23
davelens <github@davelens.be>
2025-11-21 17:16:23 +0100

    functions/elixir: Fix a bug where special characters (like `-` in "E-mail" would prevent a gettext conversion).

--- lua/config/functions/elixir.lua
+++ lua/config/functions/elixir.lua
@@ -5,8 +5,16 @@ local elixir = {}
 function elixir.wrap_with_gettext()
   local line = vim.api.nvim_get_current_line()
   local str = dvim.utils.get_visual_selection()
-  local new_line = line:gsub('"' .. str .. '"', '{gettext("' .. str .. '")}', 1)
-  vim.api.nvim_set_current_line(new_line)
+  local search = '"' .. str .. '"'
+  local replace = '{gettext("' .. str .. '")}'
+  local start_pos = line:find(search, 1, true)
+
+  if start_pos then
+    local new_line = line:sub(1, start_pos - 1)
+      .. replace
+      .. line:sub(start_pos + #search)
+    vim.api.nvim_set_current_line(new_line)
+  end
 end
 
 return elixir
