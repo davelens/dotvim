a4021d0401340e4c54e9ed6bfae0a27f466db6f8
davelens <github@davelens.be>
2025-11-28 13:25:29 +0100

    ftplugin/lua: Change number of spaces for tab chars to 2.

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,4 +1,4 @@
 vim.opt_local.expandtab = true -- Use spaces instead of tabs
-vim.opt_local.tabstop = 4 -- Number of spaces that a <Tab> counts for
+vim.opt_local.tabstop = 2 -- Number of spaces that a <Tab> counts for
 vim.opt_local.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
 vim.opt_local.softtabstop = 4 -- Number of spaces a <Tab> counts for while editing
