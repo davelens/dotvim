02173c079643047fdc01a1d932f00dc1585a2b4a
davelens <github@davelens.be>
2026-04-22 17:11:50 +0200

    plugins/active-collab: Forgot to use `opts` + correct the repo's name post-namechange.

--- lua/plugins/autoload/active-collab.lua
+++ lua/plugins/autoload/active-collab.lua
@@ -1,17 +1,19 @@
 return {
-  dir = '~/Repositories/davelens/active-collab.nvim',
+  dir = '~/Repositories/davelens/active_collab-nvim',
   name = 'active_collab.nvim',
   cmd = { 'AC' },
   keys = {
     { ';m', '<cmd>AC<cr>', desc = 'Time records' },
   },
-  account_url = 'https://next-app.activecollab.com/167099',
-  completions = {
-    filetypes = { 'html' },
+  opts = {
+    account_url = 'https://next-app.activecollab.com/167099',
+    completions = {
+      filetypes = { 'html' },
+    },
   },
   enabled = function()
     return vim.fn.isdirectory(
-      vim.fn.expand('~/Repositories/davelens/active-collab.nvim')
+      vim.fn.expand('~/Repositories/davelens/active_collab-nvim')
     ) == 1
   end,
 }
