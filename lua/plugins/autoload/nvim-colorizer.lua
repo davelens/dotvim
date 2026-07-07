a68cd7d9edcb156aeb262b2686cd40ffac24f670
davelens <github@davelens.be>
2026-07-08 01:51:43 +0200

    chore(colorizer): switch to the maintained catgoose fork
    NvChad/nvim-colorizer.lua is archived; catgoose/nvim-colorizer.lua is the
    active fork.

--- lua/plugins/autoload/nvim-colorizer.lua
+++ lua/plugins/autoload/nvim-colorizer.lua
@@ -1,6 +1,7 @@
 -- Colorize hex codes, color names, etc
 return {
-  'NvChad/nvim-colorizer.lua',
+  -- NvChad's repo is archived; catgoose maintains the active fork.
+  'catgoose/nvim-colorizer.lua',
   ft = { 'css', 'scss' },
   opts = {},
 }
